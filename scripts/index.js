4fdb33d52ad3d7150925f64f58d8881982f7f272
aleksandraVkosmose <alexandera.mir0nowa@yandex.ru>
2022-11-15 22:15:57 +0400

    исправила замечания ревьюера

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,37 @@
 let buttonEdit = document.querySelector(".profile__button-edit");
 let popup = document.querySelector(".popup");
-let container = popup.querySelector(".popup__container");
 let buttonClose = document.querySelector(".popup__button-close");
 
 buttonEdit.addEventListener('click', openPopup);
-function openPopup(){
-    popup.classList.add('popup_opened');
+function openPopup() {
+  popup.classList.add('popup_opened');
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
 };
 
 buttonClose.addEventListener('click', closePopup);
-function closePopup(){
+function closePopup() {
   popup.classList.remove('popup_opened');
 };
 
 let formElement = document.querySelector(".popup__content");
-let nameInput = document.querySelector(".popup__title");
-let jobInput = document.querySelector(".popup__subtitle");
+let nameInput = document.querySelector(".popup__form_title");
+let jobInput = document.querySelector(".popup__form_subtitle");
 let profileTitle = document.querySelector(".profile__title");
 let profileSubtitle = document.querySelector(".profile__subtitle");
 
 formElement.addEventListener('submit', formSubmitHandler);
 
-function formSubmitHandler (evt) {
+function formSubmitHandler(evt) {
   evt.preventDefault();
-   profileTitle.textContent = nameInput.value;
-   profileSubtitle.textContent = jobInput.value;
-   
-   
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
 
-   closePopup();
-   
+
+
+  closePopup();
+
 };
-   
-nameInput.value = profileTitle.textContent;
-   jobInput.value = profileSubtitle.textContent;
+
+
 
